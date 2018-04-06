9b029de1f2d45a8f4ce4517e79d86535af6ce434
Earl Platt III <36335481+eaplatt3@users.noreply.github.com>
2018-04-06 13:15:11 -0400

    PHPpratice

---
+++ php_Demo/Demo1.php
@@ -0,0 +1,16 @@
+<?php 
+echo "<h2>Hello World!</h2>";
+echo "<h2>Hello World Again!</h2>";
+echo "<h2>Hello World Once Again!</h2>";
+
+?>
+
+<html>
+<body>
+
+<?php
+
+echo "<br/>This is another PHP script";
+?>
+</body>
+</html>
---
+++ php_Demo/EchoDemo1.php
@@ -0,0 +1,11 @@
+<?php 
+
+echo "This is a text!</br>";
+
+print "This is another text!";
+
+	$data = array("Name","City","Country","Phone");
+
+print_r($data);
+
+?>
