9c2d90fe57b41a322ce913ae95ac1fa829b1470d
Earl Platt III <36335481+eaplatt3@users.noreply.github.com>
2018-04-12 14:53:12 -0400

    More Pratice PHP

---
+++ php_Demo/DataTypesDemo1.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+
+		<head>
+			<title>PHP Types!</title>
+		</head>
+<body>
+
+
+<?php 
+	
+	//string data typ is also refered to the letters A to Z
+	
+	$name = "john";
+	
+	//interger data type is referred to numeric value 0 - 9
+	
+	$age = 34; 
+	
+	//boolean data type is either true or false
+	
+	if($age == 34){
+		true;
+	}
+	
+	echo $true = true;
+	echo $false = false;
+	
+	//var_dump = lets you know what data types are the variables
+	echo var_dump($name); 
+	echo var_dump($age); 
+	echo var_dump($true);
+	
+	//Array is a data type
+	
+	$array = array("Wali", "Khan");
+	
+	echo var_dump($array);
+	
+	
+	
+
+?>
+	
+</body>
+</html>
+		
+
+<?php 
+
+	
+
+?>
---
+++ php_Demo/arraysDemo1.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+
+		<head>
+			<title>PHP Arrays!</title>
+		</head>
+<body>
+
+<?php 
+	
+	$name = "Abdul wali";
+	$name_more = array("abdul","john","london","tom","Jerry");
+	
+	echo $name_more[4];
+
+
+?>
+
+	
+</body>
+</html>
+		
+
+
